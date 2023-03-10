52240211a8c35231115637aa5fc6ffa97466e25d
ayesha <zahraayesha414@gmail.com>
2023-03-10 15:20:03 +0500

    finish chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,132 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import CryptoSummary from './Components/CryptoSummary';
+import { Crypto } from './Types';
+import type { ChartData, ChartOptions } from 'chart.js';
+import moment from 'moment';
 
-export type Crypto = {
-  ath
-  : 
-  number;
-  atl
-  : 
-  number;
-  current_price
-  : 
-  number;
-  id
-  : 
-  string;
-  name
-  : 
-  string;
-  symbol
-  : 
-  string;
-  high_24h : 
-  number;
-  low_24h : 
-  number;
-}
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 
 function App() {
-  const [cryptos, setCryptos] = useState<Crypto[] | null >();
+  const [cryptos, setCryptos] = useState<Crypto[] | null >(null);
+  const [selected, setSelected] = useState<Crypto | null >();
+  const [range, setRange] = useState<number>(30);
+  const [data, setData] = useState<ChartData<'line'>>();
+  const [options, setOptions] = useState<ChartOptions<'line'>>({
+    responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+  });
     useEffect(() => {
       const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
       axios.get(url).then((response) => {
         setCryptos(response.data);
       })
     }, []);
-        return <div className="App">
+
+    useEffect(() => {
+      if(!selected) return;
+      axios.get( 
+        `https://api.coingecko.com/api/v3/coins/${selected?.id}/market_chart?vs_currency=usd&days=${range}&${
+          range === 1 ? 'interval=hourly' : "interval=daily"}`
+        ).then((response) => { 
+          console.log(response.data);
+          setData(
+            {
+              labels: response.data.prices.map((price: number[]) => {
+                
+                return moment.unix(price[0] / 1000).format(range === 1 ? 'HH-MM' : 'MM-DD');
+              }
+              ),
+              datasets: [
+                {
+                  label: 'Dataset 1',
+                  data: response.data.prices.map((price: number[]) => {return price[1]}),
+                  borderColor: 'rgb(255, 99, 132)',
+                  backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                },
+              ],
+            });
+
+            setOptions({
+              responsive: true,
+              plugins: {
+                legend: {
+                  display: false,
+                },
+                title: {
+                  display: true,
+                  text: `${selected?.name} Price Over Last ` + range + (range === 1 
+                  ? ` Day.` 
+                  : ` Day(s).`),
+                },
+              },
+            })
+        });
+
+    }, [selected, range]);
+
+
+        return (
+        <>
+        <div className="App">
+          <select onChange={(e)=>{
+            const c = cryptos?.find((x) => x.id === e.target.value);
+            setSelected(c);
+          } }
+          
+          defaultValue = "default"
+          >
      {cryptos ? cryptos.map((crypto) => {
-      return <p>{crypto.name + ' ' + crypto.current_price}</p>
-     }) : null}
-    </div>;
+      return <option key={crypto.id} value={crypto.id}>{crypto.name}</option>;
+     }) : null }
+     <option value = 'default'>Choose an Option </option>
+     </select>
+     <select onChange={(e) => {
+
+      setRange(parseInt(e.target.value));
+      
+     }}>
+     <option value={30}>30 Days</option>
+     <option value={7}>7 Days</option>
+     <option value={1}>1 Days</option>
+     </select>
+    </div>
+    {selected ? <CryptoSummary crypto={selected} /> : null}
+   {data ? <div style= {{width:600}}>
+    <Line options={options} data={data}/></div> : null }
+    </>
+    );
+
 }
 
 export default App;
